443c458e3c0dd6781f356261a1d684cfd08a5627
ThgMelo <es.thg.melo@gmail.com>
2023-03-05 12:38:25 -0300

    Criando o ordenador

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -12,6 +12,7 @@ export default function Buscador({busca, setBusca} : BuscadorProps) {
       <input 
         value={busca}
         onChange={evento => setBusca(evento.target.value)}
+        placeholder="Buscar"
       />
       <CgSearch 
         size={20}
